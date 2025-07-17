9c1ae7822ba3fb39768d8d589b6573709637c050
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-17 14:38:43 +0530

    Create 04_datatype.c

---
+++ Chapter 1 /04_datatype.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+ int main(){
+    int a = 1;            //integer datatype  -  %d
+    float b = 14.54;        //float datatype    -  %f
+    char c = 'suraj';      //String or Character datatype   -  %c
+    printf("The value of a is %d",a);
+    printf("The value of b is %f",b);
+    printf("The value of c is %c",c);
+    return 0;
+
+
+ }
