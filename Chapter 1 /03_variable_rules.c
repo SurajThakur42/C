a616d79fc493468cf2ac6e9c4a26221a4d2873a2
SurajThakur42 <surajthakur8312@gmail.com>
2025-07-17 14:37:52 +0530

    Create 03_variable_rules.c

---
+++ Chapter 1 /03_variable_rules.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a; /*      1. First character must be an alphabet or underscore (_) 
+                   2. No commas, blanks are allowed. 
+                   3. No special symbol other than (_) allowed. 
+                   4. Variable names are case sensitive. */
+    a = 6;
+    printf("The output of this program is %d",a);
+    return 0;
+}
